c1358a839304d9aad487f94e9250a05bdf16eaf3
clarelimo <clare.limo@student.moringaschool.com>
2021-08-21 13:38:22 +0300

    create sql

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,21 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS users (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ position VARCHAR,
+ role VARCHAR,
+ departmentid INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS news (
+ id int PRIMARY KEY auto_increment,
+ post VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ description VARCHAR,
+ numberofemployees INTEGER,
+);
